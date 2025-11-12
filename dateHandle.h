875ae75f5c8a585017636cd8421c73905e310e5e
zsy642 <13846053645@163.com>
2025-11-12 23:14:22 +0800

    修改了datehandle函数头文件形参名称错误的bug

--- dateHandle.h
+++ dateHandle.h
@@ -1,6 +1,6 @@
 ﻿#ifndef DATEHANDLE_H
 #define DATEHANDLE_H
-void dateHandle(int specificmonth, int specificday, int option);
+void dateHandle(int period, int specificday, int option);
 void dateFill(int MonthlyDayInTerm[]);
 int dateMapping(int dayOfCalendar);
 void dateUpdate();
